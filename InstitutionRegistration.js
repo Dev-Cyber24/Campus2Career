3aa1d851ac83c7d8e4d3ba1efa06ff31ed9883ff
Dev-Cyber24 <ritampaul2412@gmail.com>
2026-09-04 21:42:01 +0530

    Update InstitutionRegistration.js

--- InstitutionRegistration.js
+++ InstitutionRegistration.js
@@ -4,7 +4,7 @@
 // =========================================
 
 const API_BASE_URL =
-    "http://localhost:5000/api";
+    "https://campus2career-0pi8.onrender.com/api";
 
 const REGISTER_API =
     `${API_BASE_URL}/institution/signup`;

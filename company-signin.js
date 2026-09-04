2c108ac50b0ac1527ec8eddb9710e43356b8fb00
Dev-Cyber24 <ritampaul2412@gmail.com>
2026-09-04 21:44:21 +0530

    Update company-signin.js

--- company-signin.js
+++ company-signin.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = "http://localhost:5000/api";
+const API_BASE_URL =
+    "https://campus2career-0pi8.onrender.com/api";
 
 
 // =========================================================
